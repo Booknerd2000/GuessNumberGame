4457ae52ef716ef58b6b84e65144e631a304968f
DenisAjuah <Denis.ajuah.2021@student.lhschools.org>
2019-03-07 08:14:12 -0500

    Added user input

--- src/GuessingNumber.java
+++ src/GuessingNumber.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
+
 public class GuessingNumber {
     public static void main(String[]args){
+
+        Scanner keyboard;
+        keyboard = new Scanner(System.in);
 
         int randomNumber;
         randomNumber = (int)(Math.random()*100);
         System.out.println(randomNumber);
-        
+
     }
 }
