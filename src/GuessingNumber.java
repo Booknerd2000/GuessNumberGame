33859595a067976d0bf688b213445fe67f6b5d1b
DenisAjuah <Denis.ajuah.2021@student.lhschools.org>
2019-03-07 08:10:09 -0500

    Created random number generator

--- src/GuessingNumber.java
+++ src/GuessingNumber.java
@@ -1,5 +1,9 @@
 public class GuessingNumber {
     public static void main(String[]args){
+
+        int randomNumber;
+        randomNumber = (int)(Math.random()*100);
+        System.out.println(randomNumber);
         
     }
 }
